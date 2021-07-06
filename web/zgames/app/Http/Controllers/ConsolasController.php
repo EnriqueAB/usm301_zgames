fe39403eafb0de3a5db8fdcdbfd0ea88233f1d49
EnriqueAB <enrique.alvarezb111@gmail.com>
2021-07-05 20:25:07 -0400

    Arreglar el despliegue de la tabla, y habilitar el funcionamiento del boton Eliminar

--- web/zgames/app/Http/Controllers/ConsolasController.php
+++ web/zgames/app/Http/Controllers/ConsolasController.php
@@ -43,4 +43,19 @@ class ConsolasController extends Controller
         return $consola;
     }
 
+    public function eliminarConsola(Request $request){
+        $input = $request->all();
+        $id = $input["id"];
+        //Eloquent: El administrador de BD de Laravel se llama Eloquent
+        //1. Ir a buscar el registro a la bd
+                                        //find, si el registro buscado no exite, entrega nulo
+        // $consola = App\Models\Consola::findOrFail(2)
+        $consola = Consola::findOrFail($id);//findOrFail, si el registro buscado no existe, la peticion explota...
+        //...(se puede capturar la explosion y se puede mostrar en mensaje de "Error al buscar")
+
+        //2. Para eliminar llamo al metodo delete
+        $consola->delete(); //DELETE FROM consolas WHERE id=1
+        return "ok";
+
+    }
 }
--- web/zgames/public/js/ver_consolas.js
+++ web/zgames/public/js/ver_consolas.js
@@ -1,35 +1,62 @@
+const iniciarEliminacion = async function(){
+    //1.Obtener el id a eliminar
+    let id = this.idConsola;
+    let resp = await Swal.fire({tittle:"Está seguro?", text:"Esta operación es irreversible"
+    , icon:"error", showCancelButton:true});
+    if(resp.isConfirmed){
+
+        //1. Eliminar
+        if(await eliminarConsola(id)){
+            //2. Si la eliminacion fue exitosa, recargar la tabla
+            let consolas = await getConsolas();
+            cargarTabla(consolas);
+            Swal.fire("Consola Eliminada","Consola eliminada exitosamente","info");
+        } else{
+            //3. Si no fue exitosa, mostrar mensaje de error
+            Swal.fire("Error","No se pudo eliminar la consola","error");
+        }
+    } else  {
+        Swal.fire("Cancelado","Cancelado a petición del usuario", "info");
+    }
+};
 
 const cargarTabla = (consolas)=>{
-    //
+    //1. Obtener una referencia al cuerpo de la tabla
     let tbody = document.querySelector("#tbody-consola");
-
+    tbody.innerHTML = "";
+    //2. Recorrer todas las consolas
     for(let i=0; i < consolas.length; ++i){
+        //3. Por cada consola generar una fila
         let tr = document.createElement("tr");
+        //4. Generar por cada atributo de la consola, un td
         let tdNombre = document.createElement("td");
         tdNombre.innerText = consolas[i].nombre;
         let tdMarca = document.createElement("td");
         tdMarca.innerText = consolas[i].marca;
         let tdAnio = document.createElement("td");
-        tdAnio.innerText = consolas[i].anio;
+        tdAnio.innerText  = consolas[i].anio;
         let tdAcciones = document.createElement("td");
         let botonEliminar = document.createElement("button");
-        botonEliminar.innerText = "Eliminar";
+        botonEliminar.innerText= "Eliminar";
         botonEliminar.classList.add("btn","btn-danger");
         botonEliminar.idConsola = consolas[i].id;
+        botonEliminar.addEventListener("click", iniciarEliminacion);
         tdAcciones.appendChild(botonEliminar);
-
-        tdAcciones.appendChild(tdNombre);
+        //5. Agregar los td al tr
+        tr.appendChild(tdNombre);
         tr.appendChild(tdMarca);
         tr.appendChild(tdAnio);
         tr.appendChild(tdAcciones);
-
+        //6. Agregar el tr al cuerpo de la tabla
         tbody.appendChild(tr);
     }
+
 };
 
-document.addEventListener("DOMContentLoader", async ()=>{
+
+document.addEventListener("DOMContentLoaded", async ()=>{
     //Aqui voy a cargar la tabla de consolas, porque si entra aqui
-    //lo que haga en esta parte estoy seguro que se está ejecutando
+    //lo que haga en esta parte estoy seguro que se esta ejecutando
     //cuando la pagina esta totalmente cargada
     let consolas = await getConsolas();
     cargarTabla(consolas);
