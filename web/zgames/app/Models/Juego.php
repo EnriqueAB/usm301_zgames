8889f973017b82fe40a75179bd9b37334b3c6cf9
EnriqueAB <enrique.alvarezb111@gmail.com>
2021-07-22 20:25:36 -0400

    Agregar controlador de Juegos y Modelo de Juegos

--- web/zgames/app/Models/Juego.php
+++ web/zgames/app/Models/Juego.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Consola extends Model
+class Juego extends Model
 {
     use HasFactory;
-
-    public function juegos(){
-        return $this->hasMany("App\Models\Juego", "consola_id");
+    public function consola(){
+        return $this->belongsTo("App\Models\Juego","consola_id");
     }
 
 }
